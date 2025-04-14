03aa3d3070d8a52b5402999ee16f663178f75011
alronova <aditya.divyam001@gmail.com>
2025-04-15 01:21:45 +0530

    feat: adds option to change theme, improved UI, file edits are saved to local storage, option to download the edited file.

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
+let theme = localStorage.getItem("theme");
 let edit = document.getElementById("editor");
 let preview = document.getElementById("previewer");
 window.onload = () => {
-    let markedData = marked.parse(edit.value);
-    preview.innerHTML = markedData;
-}
+  let data = localStorage.getItem("mkd-data");
+  edit.value = data;
+  let markedData = marked.parse(data);
+  preview.innerHTML = markedData;
+};
 edit.addEventListener("input", () => {
-    let markedData = marked.parse(edit.value);
-    preview.innerHTML = markedData;
-    console.log(markedData);
+  let markedData = marked.parse(edit.value);
+  preview.innerHTML = markedData;
+  console.log(markedData);
+  localStorage.setItem("mkd-data", edit.value);
 });
+
+if (theme) {
+  document.body.setAttribute("data-theme", theme);
+}
+
+let switcher = document.getElementById("theme-switch");
+let t_img = document.getElementById("theme-img");
+let d_img = document.getElementById("download-img");
+switcher.addEventListener("click", switchTheme);
+
+function switchTheme() {
+    const currentTheme = document.body.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.body.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+  if (newTheme === "dark") {
+    t_img.src = "res/sun.png";
+    d_img.src = "res/download-dark.png";
+  } else if (newTheme === "light") {
+    t_img.src = "res/moon.png";
+    d_img.src = "res/download-light.png";
+  }
+}
+
+let download = document.getElementById("download");
+download.addEventListener("click", downloadFile);
+
+function downloadFile() {
+  let data = localStorage.getItem("mkd-data");
+  let blob = new Blob([data], { type: "text/markdown" });
+  let url = URL.createObjectURL(blob);
+  let a = document.createElement("a");
+  a.href = url;
+  a.download = "edit.md";
+  a.click();
+};
